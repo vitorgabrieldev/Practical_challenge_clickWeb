677e9d7e034c4a54271d7b827be1f7a207d10db0
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2023-10-07 11:41:47 -0300

    Add Drag test

--- script.js
+++ script.js
@@ -16,17 +16,21 @@ window.addEventListener('scroll', addAnimationClass);
 
 addAnimationClass();
 
-
 const secondItem = document.querySelector('.second-item');
 
 function animateSecondItem() {
     const itemPosition = secondItem.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
-
     const margin = 100;
 
     if (itemPosition < windowHeight - margin) {
+        // Remove a classe de animação da esquerda, se estiver presente
+        secondItem.classList.remove('animate__slideInLeft');
+        
+        // Adiciona a classe de animação da direita
         secondItem.classList.add('animate__animated', 'animate__slideInRight');
+        
+        // Remove o ouvinte de evento de rolagem após a animação ser aplicada
         window.removeEventListener('scroll', animateSecondItem);
     }
 }
