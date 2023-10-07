d7b7d434814760c3750569e41ec5109b74a7d139
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2023-10-07 10:16:27 -0300

    Done Second Page

---
+++ script.js
@@ -0,0 +1,34 @@
+function addAnimationClass() {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        const itemPosition = item.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        const margin = 100;
+
+        if (itemPosition < windowHeight - margin) {
+            item.classList.add('animate__animated', 'animate__slideInLeft');
+        };
+    });
+}
+
+window.addEventListener('scroll', addAnimationClass);
+
+addAnimationClass();
+
+
+const secondItem = document.querySelector('.second-item');
+
+function animateSecondItem() {
+    const itemPosition = secondItem.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    const margin = 100;
+
+    if (itemPosition < windowHeight - margin) {
+        secondItem.classList.add('animate__animated', 'animate__slideInRight');
+        window.removeEventListener('scroll', animateSecondItem);
+    }
+}
+
+window.addEventListener('scroll', animateSecondItem);
